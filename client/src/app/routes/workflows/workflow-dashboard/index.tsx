5154eebd6d5e995edf3cae7afb41469bbd7a68fb
AleksandarTulic <aleksandartulic99@gmail.com>
2023-07-18 09:59:52 +0200

    trash can

--- client/src/app/routes/workflows/workflow-dashboard/index.tsx
+++ client/src/app/routes/workflows/workflow-dashboard/index.tsx
@@ -1,10 +1,15 @@
-import { Button, FlexRow, Icon, Switch } from "cx/widgets";
+import { Button, FlexRow, Grid, Icon, Switch } from "cx/widgets";
 
 export default () => (
     <cx>
         <div className="relative">
             <span className="relative -top-4 left-5 bg-white p-2">Morning Routine</span>
-            <Button className="absolute top-2 right-2 p-0" mod="hollow"><Icon className="h-10 w-10" name="calendar"/></Button>
+            <Button className="absolute top-2 right-2 p-0" mod="hollow">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+            </svg>
+
+            </Button>
         </div> 
 
         <div className="p-10">
@@ -43,11 +48,23 @@ export default () => (
                     </div>
                 </div>
             </div>
+
+            <div className="flex flex-col flex-1 mt-10">
+                <div class="mt-3 md:mt-0 lg:mt-0 bg-white border-2 border-gray-700 col-span-4 rounded-sm">
+                    <div className="relative">
+                        <span className="relative -top-4 left-5 bg-white p-2">Tasks</span>
+
+                        <Grid cached style={{width: "100%"}}>
+
+                        </Grid>
+                    </div> 
+                </div>
+            </div>
         </div>
     </cx>
 );
 
-const highlightEnabledBackgroundColor = "background:green";
+const highlightEnabledBackgroundColor = "background:#00FF00";
 
 const defaultStatusColor = "text-white bg-black";
 const highlightStatusColor = "text-white bg-green-600";
