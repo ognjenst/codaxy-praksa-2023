1a6021a64b07994391c612e2bb65a5a7505d8a41
marija.kravic <marija@kravic.com>
2023-07-18 13:21:09 +0200

    Data seeding not done

--- server/SOC.IoT.ApiGateway/Entities/Contexts/DeviceConfiguration.cs
+++ server/SOC.IoT.ApiGateway/Entities/Contexts/DeviceConfiguration.cs
@@ -18,7 +18,36 @@ namespace SOC.IoT.ApiGateway.Entities.Contexts
                 Manufacturer = "Philips",
                 Model = "9290022166",
                 Type = Enums.DeviceType.LIGHTBULB
-            });
+            }, new Device
+            {
+                Id = 5,
+                IoTId = "0x00124b00226969ac",
+                Name = "Contact Sensor",
+                Description = "Contact sensor",
+                Manufacturer = "SONOFF",
+                Model = "SNZB-04",
+                Type = Enums.DeviceType.SENSOR
+            }, new Device
+            {
+                Id = 6,
+                IoTId = "0xa4c13893cdd87674",
+                Name = "Wireless switch",
+                Description = "Wireless switch with 4 buttons",
+                Manufacturer = "TuYa",
+                Model = "TS0044",
+                Type = Enums.DeviceType.SENSOR
+            }, new Device
+            {
+                Id = 7,
+                IoTId = "0x00158d0001dd7e46",
+                Name = "Light bulb",
+                Description = "Smart 7W E27 light bulb",
+                Manufacturer = "Nue / 3A",
+                Model = "HGZB-06A",
+                Type = Enums.DeviceType.LIGHTBULB
+            }
+            );
+
         }
     }
 }
