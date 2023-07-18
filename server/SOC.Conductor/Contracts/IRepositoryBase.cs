dc6eb6ae26dc79298a20e9aeb76111d3c88d2c6b
marija.kravic <marija@kravic.com>
2023-07-18 16:06:42 +0200

    RepositoryBase and IRepositoryBase

---
+++ server/SOC.Conductor/Contracts/IRepositoryBase.cs
@@ -0,0 +1,10 @@
+﻿namespace SOC.Conductor.Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default);
+        Task<T> UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task<T> DeleteAsync(T entity, CancellationToken cancellationToken = default);
+        Task<T> GetAllAsync();
+    }
+}
---
+++ server/SOC.Conductor/Repositories/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+using SOC.Conductor.Contracts;
+using SOC.Conductor.Entities.Contexts;
+
+namespace SOC.Conductor.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryBase<T>
+        where T : class
+    {
+        public SOCDbContext SOCDbContext;
+        public RepositoryBase(SOCDbContext SOCDbContext)
+        {
+            this.SOCDbContext = SOCDbContext;
+        }
+        //public async Task<ICollection< T> GetAllAsync()
+        //{
+        //    return await SOCDbContext.Set<T>();
+        //}
+        public async Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
