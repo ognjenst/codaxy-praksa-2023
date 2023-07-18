6f4277ba9b604b96c474bc886e1c188cd6ea0ff5
AleksandarTulic <aleksandartulic99@gmail.com>
2023-07-18 09:11:55 +0200

    changed a little bit

--- client/src/app/routes/workflows/index.tsx
+++ client/src/app/routes/workflows/index.tsx
@@ -3,9 +3,9 @@ import WorkflowDashboard from "./workflow-dashboard";
 export default () => (
     <cx>
         <div className="m-4 flex flex-col">
-            <div class="grid grid-cols-5 gap-3">
+            <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-5 gap-3">
                 <div class="bg-blue-100">1st col</div>
-                <div class="bg-white border-2 border-gray-700 col-span-4 rounded-sm">
+                <div class="mt-3 md:mt-0 lg:mt-0 bg-white border-2 border-gray-700 col-span-4 rounded-sm">
                     <WorkflowDashboard />
                 </div>
             </div>
