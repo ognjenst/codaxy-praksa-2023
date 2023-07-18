4828d22a29b54a5155c10b45b56aba7fa16e3783
Vanja Đenadija <vanja.djenadija.pro@gmail.com>
2023-07-18 15:42:11 +0200

    Slacknet client sending message to channel

---
+++ server/SOC.Notifications/Program.cs
@@ -0,0 +1,9 @@
+﻿using SlackNet.WebApi;
+using SOC.Notifications;
+
+string accessToken = "YOUR_ACCESS_TOKEN";
+string channel = "YOUR_CHANNEL_NAME";
+string message = "Hello, Slack!";
+
+var slackService = new SlackService(accessToken);
+await slackService.SendMessage(channel, message);
---
+++ server/SOC.Notifications/SlackService.cs
@@ -0,0 +1,27 @@
+﻿using SlackNet;
+using SlackNet.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOC.Notifications
+{
+    public class SlackService
+    {
+        private readonly ISlackApiClient _slackClient;
+
+        public SlackService(string accessToken)
+        {
+            _slackClient = new SlackServiceBuilder()
+             .UseApiToken(accessToken)
+             .GetApiClient();
+        }
+
+        public async Task SendMessage(string channel, string message)
+        {
+            await _slackClient.Chat.PostMessage(new Message { Text = "Hello, Slack!", Channel = "#general" });
+        }
+    }
+}
