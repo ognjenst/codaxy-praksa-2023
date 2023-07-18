5ded2603608b1b0208ce7cf5e29651edb3548689
radusinovicmarko12 <radusinovicmarko12@gmail.com>
2023-07-18 09:44:39 +0200

    Formatting fixes

--- server/SOC.Conductor/Entities/DeviceHistory.cs
+++ server/SOC.Conductor/Entities/DeviceHistory.cs
@@ -6,13 +6,8 @@ namespace SOC.Conductor.Entities
     public class DeviceHistory
     {
         public int Id { get; set; } 
-
         public string DeviceID { get; set; }
-
         public DateTime Time { get; set; }
-
         public JObject? Configuration { get; set; }
-
-
     }
 }
