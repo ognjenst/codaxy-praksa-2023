0cb056aba36321cc1df416c527244bfe6359c2b7
Vanja Đenadija <vanja.djenadija.pro@gmail.com>
2023-07-18 10:24:18 +0200

    Added State and Color components

--- client/src/app/routes/devices/device-details/Controller.ts
+++ client/src/app/routes/devices/device-details/Controller.ts
@@ -1,19 +1,18 @@
 import { Controller } from "cx/ui";
-import { GET } from '../../../api/util/methods';
+import { GET } from "../../../api/util/methods";
 
 export default class extends Controller {
     onInit(): void {
         this.loadData();
-     }
-  
-     async loadData() {
-        let id = this.store.get('$route.id');
-       try{
-         let resp = await GET(`/devices/${id}`);
-         this.store.set('$page.device', resp);
-         console.log(this.store.get('$page.device'));
-       } catch(err){
-         console.error(err);
-       }
-     }
+    }
+
+    async loadData() {
+        let id = this.store.get("$route.id");
+        try {
+            let resp = await GET(`/devices/${id}`);
+            this.store.set("$page.device", resp);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 }
--- client/src/app/routes/devices/device-details/index.tsx
+++ client/src/app/routes/devices/device-details/index.tsx
@@ -1,13 +1,23 @@
+import { PureContainer } from "cx/ui";
 import { Heading } from "cx/widgets";
 import Controller from "./Controller";
 import { BrightnessComponent } from "./BrightnessComponent";
+import { StateComponent } from "./StateComponent";
 
 export default () => (
     <cx>
-        <div controller={Controller}>
-            <Heading level="1" text-bind="$page.device.id" />
-            <div visible-bind="$page.device.light">
-                <BrightnessComponent />
+        <div controller={Controller} className="p-4">
+            <Heading level="1" text-bind="$page.device.id" className="text-4xl" />
+            <div className="p-8">
+                <div visible-bind="$page.device.state">
+                    <StateComponent />
+                </div>
+                <div visible-bind="$page.device.light">
+                    <BrightnessComponent />
+                </div>
+                <div visible-bind="$page.device.colorXy">
+                    <BrightnessComponent />
+                </div>
             </div>
         </div>
     </cx>
