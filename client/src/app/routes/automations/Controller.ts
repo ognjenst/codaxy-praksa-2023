148cd06b3017f8b18ed01d0f35e1170c721477b4
tepictanja <tepictanja1146@gmail.com>
2023-07-18 10:20:24 +0200

    Automations

--- client/src/app/routes/automations/Controller.ts
+++ client/src/app/routes/automations/Controller.ts
@@ -2,6 +2,18 @@ import { Controller } from "cx/ui";
 
 export default class extends Controller{
     onInit(){
+        //this.store.set("$page.trigger", "triger");
+    }
+
+    async loadData(){
+        
+    }
+
+    addAutomation(){
+
+    }
+
+    addTrigger(){
 
     }
 }
--- client/src/app/routes/automations/index.tsx
+++ client/src/app/routes/automations/index.tsx
@@ -1,24 +1,61 @@
-import { List, LookupField, PureContainer } from "cx/widgets";
+import { LabelsLeftLayout, UseParentLayout } from "cx/ui";
+import { Button, DateTimeField, LookupField, PureContainer, Section, TextField } from "cx/widgets";
+import { encodeDateWithTimezoneOffset } from "cx/util";
 import Controller from "./Controller";
 
-export default  () => (
+export default () => (
     <cx>
         <PureContainer controller={Controller}>
             <div className="flex">
-                <div className="">
-                    <List>
+                <Section title="Add automation">
+                    <div className="flex-1">
+                        <LabelsLeftLayout>
+                            <LookupField label="Select trigger" options={triggers} value-bind="$page.automations.triggers" />
+                            <LookupField label="Select workflow" options={workflows} value-bind="$page.automations.workflows" />
+                            <Button text="Add automation" icon="plus" mod="primary" />
+                        </LabelsLeftLayout>
+                    </div>
+                </Section>
+                <Section title="Add trigger">
+                    <div className="flex-1">
+                        <LabelsLeftLayout>
+                            <LookupField label="Trigger type" options={options} optionTextField="name" value-bind="$page.trigger.type" />
 
-                    </List>
-                </div>
-                <div className="flex-1">
-                    <LookupField label ="Trigger type" options={options} text-bind ="$page.options.text"/>
-                </div>
+                            <PureContainer layout={UseParentLayout} visible-expr="{$page.trigger.type} == 1">
+                                <DateTimeField
+                                    encoding={encodeDateWithTimezoneOffset}
+                                    value-bind="$page.trigger.start"
+                                    label="Start"
+                                    required
+                                />
+                                <TextField value-bind="$page.trigger.period" label="Period" required />
+                                <TextField value-bind="$page.trigger.unit" label="Unit" required />
+                            </PureContainer>
+                            <PureContainer layout={UseParentLayout} visible-expr="{$page.trigger.type} == 2">
+                                <TextField value-bind="$page.trigger.property" label="Property" required />
+                                <TextField value-bind="$page.trigger.value" label="Value" required />
+                                <TextField value-bind="$page.trigger.condition" label="Condition" required />
+                            </PureContainer>
+                            <Button text="Add trigger" icon="plus" mod="primary" />
+                        </LabelsLeftLayout>
+                    </div>
+                </Section>
             </div>
         </PureContainer>
     </cx>
 );
 
 const options = [
-    {id: 1, text: 'Periodic Trigger'},
-    {id: 2, text: 'IoT Trigger'}
+    { id: 1, name: "Periodic Trigger" },
+    { id: 2, name: "IoT Trigger" },
+];
+
+const triggers = [
+    { id: 1, text: "Trigger 1" },
+    { id: 2, text: "Trigger 2" },
+];
+
+const workflows = [
+    { id: 1, text: "Workflow 1" },
+    { id: 2, text: "Workflow 2" },
 ];
