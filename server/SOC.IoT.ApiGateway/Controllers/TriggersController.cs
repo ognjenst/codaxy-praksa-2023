925e6bd0a1844d1127702c9e9e4f79b899dfdfab
tepictanja <111061395+tepictanja@users.noreply.github.com>
2023-07-19 14:26:02 +0200

    Feat/master/p2023 17 Create Automations page (#9)
    * Lookup
    
    * Automations
    
    * Workflows
    
    * Triggers and Automation
    
    * Triggers and Automation back
    
    * Automations page
    
    * descriptive annotations and comments
    
    * annotations and comments
    
    * format file

---
+++ server/SOC.IoT.ApiGateway/Controllers/TriggersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SOC.Conductor.Client.Generated;
+
+namespace SOC.IoT.ApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TriggersController : ControllerBase
+    {
+        private readonly ITriggersService _triggersService;
+
+        public TriggersController(ITriggersService triggersService)
+        {
+            _triggersService = triggersService;
+        }
+
+
+        /// <summary>
+        /// Returns all triggers.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "GetAllTriggersAsync")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IList<CommonTriggerDto>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = null)]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = null)]
+        public async Task<IActionResult> GetAllTriggersAsync()
+        {
+            var triggers = await _triggersService.GetAllTriggersAsync();
+
+            await Task.Delay(1000);
+
+            return Ok(triggers);
+        }
+    }
+}
