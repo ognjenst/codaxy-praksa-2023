36b04c98d4f1d5350a8fdb35a881dbb2edbb0c01
Vanja Đenadija <vanja.djenadija.pro@gmail.com>
2023-07-19 14:13:33 +0200

    Create notification microservice done

--- server/SOC.Notifications/Program.cs
+++ server/SOC.Notifications/Program.cs
@@ -1,9 +1,72 @@
-﻿using SlackNet.WebApi;
+﻿using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Microsoft.Extensions.Hosting;
+using SlackNet.WebApi;
 using SOC.Notifications;
+using Microsoft.Extensions.Configuration;
+using ConductorSharp.Engine.Extensions;
+using ConductorSharp.Engine.Health;
+using MediatR.Extensions.Autofac.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using SOC.Notifications.Extensions;
+using SOC.Notifications.Services;
+using Autofac.Core;
+using SOC.Notifications.Handler;
 
-string accessToken = "YOUR_ACCESS_TOKEN";
-string channel = "YOUR_CHANNEL_NAME";
-string message = "Hello, Slack!";
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(
+        (hosting, config) =>
+        {
+            config.AddJsonFile("appsettings.Development.json");
+        }
+    )
+    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureServices(
+        (context, services) =>
+        {
+            services.RegisterServices(context.Configuration);
+        }
+    ).
+    ConfigureContainer<ContainerBuilder>(
+        (context, builder) =>
+        {
+            builder
+                .AddConductorSharp(
+                    baseUrl: context.Configuration.GetValue<string>("Conductor:BaseUrl"),
+                    apiPath: context.Configuration.GetValue<string>("Conductor:ApiUrl")
+                )
+                .AddExecutionManager(
+                    maxConcurrentWorkers: context.Configuration.GetValue<int>(
+                        "Conductor:MaxConcurrentWorkers"
+                    ),
+                    sleepInterval: context.Configuration.GetValue<int>("Conductor:SleepInterval"),
+                    longPollInterval: context.Configuration.GetValue<int>(
+                        "Conductor:LongPollInterval"
+                    ),
+                    domain: context.Configuration.GetValue<string>("Conductor:WorkerDomain"),
+                    handlerAssemblies: typeof(Program).Assembly
+                )
+                .SetHealthCheckService<FileHealthService>()
+                .AddPipelines(pipelines =>
+                {
+                    pipelines.AddContextLogging();
+                    pipelines.AddRequestResponseLogging();
+                    pipelines.AddValidation();
+                });
 
-var slackService = new SlackService(accessToken);
-await slackService.SendMessage(channel, message);
+            builder.RegisterWorkerTask<SendSlackMessageHandler>();
+
+            builder.RegisterMediatR(typeof(Program).Assembly);
+        }
+    );
+
+using var host = builder.Build();
+
+/*var slackService = host.Services.GetRequiredService<ISlackService>();
+
+string channel = "#soc";
+string message = "This message is formated using SlackNet.";
+await slackService.SendMessage(message);*/
+
+await host.RunAsync();
+
---
+++ server/SOC.Notifications/Services/SlackService.cs
@@ -0,0 +1,26 @@
+﻿using SlackNet;
+using SlackNet.Blocks;
+using SlackNet.WebApi;
+using static SOC.Notifications.Util.Util;
+
+namespace SOC.Notifications.Services
+{
+    public class SlackService : ISlackService
+    {
+        private readonly ISlackApiClient _slackClient;
+        private readonly string _slackChannel;
+        private readonly string _slackContextBlockImageUrl;
+
+        public SlackService(string accessToken, string slackChannel, string slackContextBlockImageUrl)
+        {
+            _slackClient = new SlackServiceBuilder().UseApiToken(accessToken).GetApiClient();
+            _slackChannel = slackChannel;
+            _slackContextBlockImageUrl = slackContextBlockImageUrl;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await _slackClient.Chat.PostMessage(new Message { Channel = _slackChannel , Blocks = GetSlackBlocks(message, _slackContextBlockImageUrl) });
+        }
+    }
+}
