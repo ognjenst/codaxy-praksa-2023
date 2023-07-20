5996bcd01432f8a20ba669b5c22fc109df74b611
milicakoprena <milicakoprena00@gmail.com>
2023-07-20 12:32:16 +0200

    ITicketingService with Crud operations for cases, tasks and alerts + TicketingService

--- server/SOC.Ticketing/Services/TicketingService.cs
+++ server/SOC.Ticketing/Services/TicketingService.cs
@@ -9,62 +9,62 @@ namespace SOC.Ticketing.Services
 {
     public class TicketingService : ITicketingService
     {
-        public Task<OutputAlert> CreateAlertAsync(InputCreateAlert inputCreateAlert)
+        public Task<OutputAlert> CreateAlertAsync(InputCreateAlert inputCreateAlert, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<OutputCase> CreateCaseAsync(InputCreateCase inputCreateCase)
+        public Task<OutputCase> CreateCaseAsync(InputCreateCase inputCreateCase, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<OutputTask> CreateTaskAsync(InputCreateTask inputCreateTask)
+        public Task<OutputTask> CreateTaskAsync(InputCreateTask inputCreateTask, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> DeleteAlertAsync(int id)
+        public Task<Response> DeleteAlertAsync(int id, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> DeleteCaseAsync(int id)
+        public Task<Response> DeleteCaseAsync(int id, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> DeleteTaskAsync(int id)
+        public Task<Response> DeleteTaskAsync(int id, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<OutputAlert> GetAlertAsync(int id)
+        public Task<OutputAlert> GetAlertAsync(int id, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<OutputCase> GetCaseAsync(int id)
+        public Task<OutputCase> GetCaseAsync(int id, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<OutputTask> GetTaskAsync(int id)
+        public Task<OutputTask> GetTaskAsync(int id, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> UpdateAlertAsync(InputUpdateAlert inputUpdateAlert)
+        public Task<Response> UpdateAlertAsync(InputUpdateAlert inputUpdateAlert, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> UpdateCaseAsync(InputUpdateCase inputUpdateCase)
+        public Task<Response> UpdateCaseAsync(InputUpdateCase inputUpdateCase, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Response> UpdateTaskAsync(InputUpdateTask inputUpdateTask)
+        public Task<Response> UpdateTaskAsync(InputUpdateTask inputUpdateTask, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
