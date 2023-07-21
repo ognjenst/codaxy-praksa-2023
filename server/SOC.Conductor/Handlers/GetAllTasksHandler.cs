451991f6947b54b0538ef622279e99a1370e0a9d
AleksandarTulic <aleksandartulic99@gmail.com>
2023-07-21 10:09:36 +0200

    added handler and profile - automapper

---
+++ server/SOC.Conductor/Handlers/GetAllTasksHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using SOC.Conductor.Generated;
+using SOC.Conductor.Models;
+using SOC.Conductor.Models.Requests;
+
+namespace SOC.Conductor.Handlers
+{
+    public class GetAllTasksHandler : IRequestHandler<GetAllTasks, List<TaskResponseDto>>
+    {
+        private readonly IMetadataResourceClient _client;
+        private readonly IMapper _mapper;
+
+        public GetAllTasksHandler(IMetadataResourceClient client, IMapper mapper)
+        {
+            _client = client;
+            _mapper = mapper;
+        }
+
+        public async Task<List<TaskResponseDto>> Handle(GetAllTasks request, CancellationToken cancellationToken)
+        {
+            var arrTasks = await _client.GetTaskDefsAsync();
+
+            var arrReturn = new List<TaskResponseDto>();
+        }
+    }
+}
