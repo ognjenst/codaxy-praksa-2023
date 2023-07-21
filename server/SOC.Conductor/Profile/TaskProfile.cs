42b64651b50544eb2f7e855cd476ddd7ef7c297f
AleksandarTulic <aleksandartulic99@gmail.com>
2023-07-21 10:13:54 +0200

    added handler and profile - automapper

---
+++ server/SOC.Conductor/Profile/TaskProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SOC.Conductor.Generated;
+using SOC.Conductor.Models;
+
+namespace SOC.Conductor.Profile
+{
+    public class TaskProfile : AutoMapper.Profile
+    {
+        public TaskProfile()
+        {
+            CreateMap<TaskDef, TaskResponseDto>();
+        }
+    }
+}
