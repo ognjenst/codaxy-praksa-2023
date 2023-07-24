f13c180da9f4b2bdf6d9a9304832cd28e30cc48e
sergejsoldat20 <sergej.s.soldat20@gmail.com>
2023-07-24 12:31:32 +0200

    required changes done

--- server/SOC.Conductor/Controllers/WorkflowsController.cs
+++ server/SOC.Conductor/Controllers/WorkflowsController.cs
@@ -94,6 +94,56 @@ public class WorkflowsController : ControllerBase
     }
 
 	/// <summary>
+	/// Get all tasks 
+	/// </summary>
+	/// <param name=""></param>
+	/// <returns></returns>
+	[HttpGet("GetAllTasks", Name = "GetAllTasks")]
+	[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ICollection<TaskResponseDto>))]
+	[ProducesResponseType(StatusCodes.Status400BadRequest, Type = null)]
+	[ProducesResponseType(StatusCodes.Status201Created, Type = null)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError, Type = null)]
+	public async Task<IActionResult> GetAllTasksAsync()
+	{
+		var response = await _mediator.Send(new GetAllTasks());
+		if (response == null) return NotFound();
+		return Ok(response);
+	}
+
+	/// <summary>
+	/// Get all workflows
+	/// </summary>
+	/// <param name=""></param>
+	/// <returns></returns>
+	[HttpGet("GetAllWorkflows", Name = "GetAllWorkflows")]
+	[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ICollection<WorkflowResponseDto>))]
+	[ProducesResponseType(StatusCodes.Status400BadRequest, Type = null)]
+	[ProducesResponseType(StatusCodes.Status201Created, Type = null)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError, Type = null)]
+	public async Task<IActionResult> GetAllWorkflows()
+	{
+		var response = await _mediator.Send(new GetAllWorkflows());
+		if (response == null) return NotFound();
+		return Ok(response);
+	}
+
+	[HttpPut("PauseWorkflowAsync")]
+	public async Task<IActionResult> PauseWorkflowAsync([FromBody] PauseWorkflowRequestDto pauseDto)
+	{
+		await _mediator.Send(new PauseWorkflow(pauseDto));
+
+		return NoContent();
+	}
+
+	[HttpPut("ResumeWorkflowAsync")]
+	public async Task<IActionResult> ResumeWorkflowAsync([FromBody] ResumeWorkflowRequestDto resumeDto)
+	{
+		await _mediator.Send(new ResumeWorkflow(resumeDto));
+
+		return NoContent();
+	}
+
+	/// <summary>
 	/// Play workflow
 	/// </summary>
 	/// <param name="playDto"></param>
