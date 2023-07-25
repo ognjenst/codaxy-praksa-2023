6c42982e12c4f45dc755555441fcbb5d8c45c4c5
darkobuvac <darko.buvac@codaxy.com>
2023-07-25 21:46:09 +0200

    add and configure docker files for each project

--- server/SOC.Scanning/Program.cs
+++ server/SOC.Scanning/Program.cs
@@ -4,9 +4,7 @@ using ConductorSharp.Engine.Extensions;
 using ConductorSharp.Engine.Health;
 using MediatR.Extensions.Autofac.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SOC.Scanning;
 using SOC.Scanning.Extensions;
 using SOC.Scanning.Handler;
 
@@ -58,5 +56,4 @@ var builder = Host.CreateDefaultBuilder()
     );
 
 using var host = builder.Build();
-
 await host.RunAsync();
