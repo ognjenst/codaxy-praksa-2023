2e76948119e449d3dfebcee29e9e14c40de1c870
darkobuvac <darko.buvac@codaxy.com>
2023-07-25 14:02:41 +0200

    make prop nullable

---
+++ server/SOC.Conductor/Migrations/20230725120217_InputParamsNullable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOC.Conductor.Migrations
+{
+    /// <inheritdoc />
+    public partial class InputParamsNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Automations",
+                table: "Automations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InputParameters",
+                table: "Automations",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "jsonb");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Automations",
+                table: "Automations",
+                columns: new[] { "WorkflowId", "TriggerId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Automations",
+                table: "Automations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InputParameters",
+                table: "Automations",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "jsonb",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Automations",
+                table: "Automations",
+                columns: new[] { "WorkflowId", "TriggerId", "Name" });
+        }
+    }
+}
