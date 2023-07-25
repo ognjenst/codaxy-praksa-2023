5ad84dd2979d06363d7473e84359f9e28eda11d8
Codaxy <teodorafus@hotmail.com>
2023-07-25 14:43:13 +0200

    Added SSH client service and method for scanning IP addresses

---
+++ server/SOC.Scanning/Services/ISshClientService.cs
@@ -0,0 +1,15 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOC.Scanning.Services
+{
+    public interface ISshClientService
+    {
+        public SshCommand ExecuteCommand(string command);
+        public void Connect();
+    }
+}
