3e8a905eb31305d54964d136e682a4c539f80573
darkobuvac <darko.buvac@codaxy.com>
2023-08-02 12:31:51 +0200

    fix bug

--- server/SOC.IoT/Program.cs
+++ server/SOC.IoT/Program.cs
@@ -12,19 +12,22 @@ using SOC.IoT.Services;
 
 var builder = Host.CreateDefaultBuilder()
     .ConfigureAppConfiguration(
-        (hosting, config) => {
+        (hosting, config) =>
+        {
             config.AddJsonFile("appsettings.json", true);
             config.AddJsonFile("appsettings.Development.json", true);
         }
     )
     .UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureServices(
-        (context, services) => {
+        (context, services) =>
+        {
             services.RegisterServices(context.Configuration);
         }
     )
     .ConfigureContainer<ContainerBuilder>(
-        (context, builder) => {
+        (context, builder) =>
+        {
             builder
                 .AddConductorSharp(
                     baseUrl: context.Configuration.GetValue<string>("Conductor:BaseUrl"),
@@ -42,18 +45,17 @@ var builder = Host.CreateDefaultBuilder()
                     handlerAssemblies: typeof(Program).Assembly
                 )
                 .SetHealthCheckService<FileHealthService>()
-                .AddPipelines(pipelines => {
+                .AddPipelines(pipelines =>
+                {
                     pipelines.AddContextLogging();
                     pipelines.AddRequestResponseLogging();
                     pipelines.AddValidation();
                 });
 
             builder.RegisterWorkerTask<DeviceHandler>();
-			builder.RegisterWorkerTask<DetectionHandler>();
+            builder.RegisterWorkerTask<DetectionHandler>();
             builder.RegisterWorkerTask<CheckTimeHandler>();
-		}
             builder.RegisterWorkerTask<TemperatureColorHandler>();
-
         }
     );
 
