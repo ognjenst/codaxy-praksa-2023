bf3c9d489ff3807018d55b6b7fba3f1fc09064d2
Marko Radusinovic <74877695+radusinovicmarko@users.noreply.github.com>
2023-08-02 09:51:28 +0200

    Task P2023-42 Temperature Color Change Automation (#45)
    * Temperature color task handler created and registered
    
    * appsettings
    
    * Device client regenerated
    
    * Added Color constants

--- server/SOC.IoT/Program.cs
+++ server/SOC.IoT/Program.cs
@@ -48,6 +48,7 @@ var builder = Host.CreateDefaultBuilder()
                 });
 
             builder.RegisterWorkerTask<DeviceHandler>();
+            builder.RegisterWorkerTask<TemperatureColorHandler>();
 
         }
     );
